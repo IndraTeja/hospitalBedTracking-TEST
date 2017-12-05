230b851a6ed9b4badfc42fe80bf4f5a6a3f56caf
indraTeja <indrateja@gmail.com>
2017-12-04 23:02:15 -0600

    Indra press_report

---
+++ press_report.py
@@ -0,0 +1,47 @@
+#Author : Indra Teja Chintakayala
+#Assignment 4
+#ISQA8210
+
+
+#modules
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class Test_1(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_1(self):
+        # user = "instructor"
+        # pwd = "instructor1a"
+        driver = self.driver
+        driver.maximize_window()
+        driver.get("https://durangopy-bed-track.herokuapp.com/")
+        time.sleep(3)
+        driver.get("https://durangopy-bed-track.herokuapp.com/press_report/")
+        time.sleep(3)
+        elem = driver.find_element_by_id("myInput")
+        elem.send_keys("University")
+        time.sleep(5)
+        elem.clear()
+        elem = driver.find_element_by_id("myInput")
+        elem.send_keys("Children")
+        time.sleep(5)
+        elem.clear()
+        elem = driver.find_element_by_id("myInput")
+        elem.send_keys("Select")
+        time.sleep(3)
+        # assert "Logged In"
+        # time.sleep(3)
+        # elem = driver.find_element_by_xpath("/html/body/div/div[2]/div[1]/div[3]/table/tbody/tr[2]/th/a")
+        # elem.click()
+        # time.sleep(3)
+
+    def tearDown(self):
+                self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test_demo.py
@@ -0,0 +1,38 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import csv
+from press_report import *
+from tc2 import *
+
+
+class ConfigTestCase(unittest.TestCase):
+    def setUp(self):
+        print('set up')
+
+    def runTest(self):
+        print('run test')
+
+# def suite():
+#     # test_suite = unittest.TestSuite()
+#
+#
+#     tc_1 = unittest.TestLoader().loadTestsFromTestCase(HomeTest_1)
+#     # tc_2 = unittest.TestLoader().loadTestsFromTestCase(HomeTest_2)
+#
+#     # create a test suite combining search_text and home_page_test
+#     test_suite = unittest.TestSuite([tc_1])
+#
+#     # test_suite.addTest(unittest.loader.findTestCases(tc1))
+#     # test_suite.addTest(unittest.loader.findTestCases(tc2))
+#
+#     return test_suite
+#
+#
+# mySuit = suite()
+#
+# runner = unittest.TextTestRunner()
+# runner.run(mySuit)
+
+# unittest.TextTestResult().run(mySuit)
